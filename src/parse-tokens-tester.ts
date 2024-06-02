0259be70de168e8df888d1b1a9f3fcb1511b7ebb
Nixinova <nixinova@gmail.com>
2024-06-03 03:00:02 +1200

    Add all parser functions

--- src/parse-tokens-tester.ts
+++ src/parse-tokens-tester.ts
@@ -2,13 +2,9 @@ import { parse } from './parse-tokens';
 
 console.log(parse(`
 
-<ref>data</ref>
-<ref group=note>note</ref>
-<ref name=":0">Content</ref>
-<ref name=":0"></ref>
-
-==refs==
-<references/>
-<references group=note/>
+{{#titleparts: Talk:Foo/bar/baz/quok }} → Talk:Foo/bar/baz/quok
+{{#titleparts: Talk:Foo/bar/baz/quok | 1 }} → Talk:Foo
+{{#titleparts: Talk:Foo/bar/baz/quok | -1 }} → Talk:Foo/bar/baz
+{{#titleparts: Talk:Foo/bar/baz/quok | -4 }} → .
 
 `).data)
--- src/parse-tokens.ts
+++ src/parse-tokens.ts
@@ -70,6 +70,7 @@ function parseToken(token: AnyToken): string {
             return `<a class="external-link" href="${token.firstChild}">${token.innerText}</a>`;
         },
         'file': () => {
+            // TODO frameless/border/thumb/etc; float; align, alt, style, class, upright 
             const width = token.width || 'auto';
             const height = token.height || 'auto';
             const file = token.name;
@@ -78,22 +79,289 @@ function parseToken(token: AnyToken): string {
             return img;
         },
         'table': () => {
-            const [_open, attrs, ...cells] = token.childNodes;
-            const body = cells.map((token: AnyToken) => parseToken(token)).join('');
-            return htmlElem('table', parseToken(attrs), body);
+            const [_open, attrs, ...cells] = token.childNodes.map(parseToken);
+            return htmlElem('table', attrs, cells.join(''));
         },
         'tr': () => {
-            const [_open, attrs, ...cells] = token.childNodes;
-            const body = cells.map((token: AnyToken) => parseToken(token)).join('');
-            return htmlElem('tr', parseToken(attrs), body);
+            const [_open, attrs, ...cells] = token.childNodes.map(parseToken);
+            return htmlElem('tr', attrs, cells.join(''));
         },
         'td': () => {
             const { subtype } = token;
-            const [_open, attrs, ...cells] = token.childNodes;
-            const body = cells.map((token: AnyToken) => parseToken(token)).join('');
-            return htmlElem(subtype, parseToken(attrs), body);
+            const [_open, attrs, ...cells] = token.map(parseToken);
+            return htmlElem(subtype, attrs, cells.join(''));
         },
         'table-syntax': () => '',
+        'double-underscore': () => {
+            // __NOTOC__, __NOINDEX__, etc
+            const { name } = token;
+            metadata[name] = true;
+            return htmlElem(name);
+        },
+        'magic-word': () => {
+            const [_nameToken, ...paramTokens]: string[] = token.childNodes;
+            const [_name, ...params]: string[] = token.childNodes.map(parseToken)
+                .map((item: string) => /^\s+$/.test(item) ? ' ' : item.trim());
+            const [param] = params;
+            const d = new Date();
+            switch (token.name) {
+                // Parser functions: built-in
+                case 'lc': {
+                    // @changelog CHANGE: no longer allows '#lc'
+                    return param.toLowerCase();
+                }
+                case 'uc': {
+                    // @changelog CHANGE: no longer allows '#uc'
+                    return param.toUpperCase();
+                }
+                case 'lcfirst': {
+                    // @changelog CHANGE: no longer allows '#'
+                    return param[0].toLowerCase() + param.slice(1);
+                }
+                case 'ucfirst': {
+                    // @changelog CHANGE: no longer allows '#'
+                    return param[0].toUpperCase() + param.slice(1);
+                }
+                case 'padleft': {
+                    const [text, size, filler] = params;
+                    // @changelog CHANGE: no longer allows '#'
+                    return text.padStart(+size, filler);
+                }
+                case 'padright': {
+                    const [text, size, filler] = params;
+                    // @changelog CHANGE: no longer allows '#'
+                    return params[0].padEnd(+size, filler);
+                }
+                case 'urlencode': {
+                    // @changelog CHANGE: no longer allows '#'
+                    // @changelog REMOVE urldecode
+                    return encodeURIComponent(param);
+                }
+                case 'urldecode': {
+                    // @changelog CHANGE: now requires '#'
+                    return decodeURIComponent(param);
+                }
+                // Parser functions: string functions
+                case 'len': {
+                    return param.length.toString();
+                }
+                case 'pos': {
+                    const [text, substring] = params;
+                    return text.indexOf(substring).toString();
+                }
+                case 'rpos': {
+                    // @changelog NEW
+                    const [text, substring] = params;
+                    return text.lastIndexOf(substring).toString();
+                }
+                case 'sub': {
+                    const [text, startstr, lengthstr] = params;
+                    const start = +startstr;
+                    const length = +lengthstr;
+                    if (start < 0) {
+                        if (!length) {
+                            // {{#sub:texting|-2}}=ng
+                            return text.slice(text.length + start);
+                        }
+                        else if (length < 0) {
+                            // {{#sub:texting|-2|-1}}=n_
+                            return text.slice(text.length + start, text.length + length);
+                        }
+                        else {
+                            // {{#sub:texting|-2|2}}=ng
+                            return text.slice(text.length + start, text.length + start + length);
+                        }
+                    }
+                    else {
+                        if (!length) {
+                            // {{#sub:texting|2}}=__xting
+                            return text.slice(start);
+                        }
+                        else if (length < 0) {
+                            // {{#sub:texting|2|-2}}=__xti__
+                            return text.slice(start, text.length + length);
+                        }
+                        else {
+                            // {{#sub:texting|2|2}}=__xt
+                            return text.slice(start, start + length);
+                        }
+                    }
+                }
+                case 'count': {
+                    // @changelog NEW
+                    const [text, substring] = params;
+                    return (text.split(substring).length - 1).toString();
+                }
+                case 'replace': {
+                    const [text, search, replacer] = params;
+                    return text.replaceAll(search, replacer);
+                }
+                case 'explode': {
+                    const [text, delimiter, pos, limit] = params;
+                    let arr = text.split(delimiter);
+                    if (limit) {
+                        // Joins all items in the array after the limit length into the same cell
+                        arr[+limit - 1] = arr.slice(+limit - 1).join(delimiter);
+                        arr.length = +limit;
+                    }
+                    return arr[+pos];
+                }
+                // Parser functions: extension
+                case 'expr': {
+                    // TODO
+                    return htmlElem('UNIMPLEMENTED', `expr="${param}"`, '');
+                }
+                case 'if': {
+                    const [check, iftrue = '', iffalse = ''] = params;
+                    const result = check.trim() ? iftrue : iffalse;
+                    return result ?? '';
+                }
+                case 'ifeq': {
+                    const [check1, check2, iftrue = '', iffalse = ''] = params;
+                    if (!isNaN(+check1) && !isNaN(+check2)) {
+                        // numerical check
+                        return +check1 === +check2 ? iftrue : iffalse;
+                    } else {
+                        // text check
+                        return check1.trim() === check2.trim() ? iftrue : iffalse;
+                    }
+                }
+                case 'iferror': {
+                    const [check, iftrue = '', iffalse = ''] = params;
+                    return check.includes(`class="error"`) ? iftrue : iffalse;
+                }
+                case 'ifexpr': {
+                    // TODO
+                    return htmlElem('UNIMPLEMENTED', `ifexpr="${param}"`, '');
+                }
+                case 'ifexist': {
+                    // TODO
+                    const [check, iftrue = '', iffalse = ''] = params;
+                    return iftrue;
+                }
+                case 'rel2abs': {
+                    // TODO
+                    const [path, basePath] = params;
+                    return path;
+                }
+                case 'switch': {
+                    const [_itemToken, ...optionsTokens] = paramTokens;
+                    const [item] = params;
+                    const options = optionsTokens.map((token: AnyToken) => parseToken(token.firstChild) + '=' + parseToken(token.lastChild));
+                    for (const option of options) {
+                        const [key, value] = option.split('=');
+                        if (item.trim() === key.trim()) {
+                            return value;
+                        }
+                    }
+                    return '';
+                }
+                case 'time': {
+                    const [fmtstr, datetime, langcode, local] = params;
+                    return htmlElem('UNIMPLEMENTED', `str="${fmtstr}" datetime="${datetime}" lang="${langcode}" local="${local}"`, '');
+                }
+                case 'timel': {
+                    const [fmtstr, datetime, langcode] = params;
+                    return htmlElem('UNIMPLEMENTED', `str="${fmtstr}" datetime="${datetime}" lang="${langcode}" local="true"`, '');
+                }
+                case 'timel': {
+                    const [fmtstr, datetime, langcode] = params;
+                    return htmlElem('UNIMPLEMENTED', `str="${fmtstr}" datetime="${datetime}" lang="${langcode}" local="true"`, '');
+                }
+                case 'titleparts': {
+                    const [pagename, count, startSegment] = params;
+                    const parts = pagename.split('/');
+                    if (!count) {
+                        return pagename;
+                    }
+                    else if (!startSegment) {
+                        return parts.slice(0, +count).join('/');
+                    }
+                    else {
+                        return parts.slice(+startSegment - 1, +count).join('/');
+                    }
+                }
+                // Magic words
+                case 'pagename': {
+                    // @changelog NEW
+                    return htmlElem('UNIMPLEMENTED');
+                }
+                case 'currentyear':
+                case 'localyear': {
+                    return d.getFullYear().toString();
+                }
+                case 'currentmonth':
+                case 'currentmonth2':
+                case 'localmonth':
+                case 'localmonth2': {
+                    return (d.getMonth() + 1).toString().padStart(2, '0');
+                }
+                case 'currentmonth1':
+                case 'localmonth1': {
+                    return (d.getMonth() + 1).toString();
+                }
+                case 'currentmonthname':
+                case 'currentmonthnamegen':
+                case 'localmonthname':
+                case 'localmonthnamegen': {
+                    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                    return months[d.getMonth()];
+                }
+                case 'currentmonthabbrev':
+                case 'localmonthabbrev': {
+                    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+                    return months[d.getMonth()];
+                }
+                case 'currentday':
+                case 'localday': {
+                    return d.getDate().toString();
+                }
+                case 'currentday2':
+                case 'localday2': {
+                    return d.getDate().toString().padStart(2, '0');
+                }
+                case 'currentdow':
+                case 'localdow': {
+                    return d.getDay().toString();
+                }
+                case 'currentdow':
+                case 'localdow': {
+                    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                    return days[d.getDay()];
+                }
+                case 'currenttime':
+                case 'localtime': {
+                    return [d.getHours(), d.getMinutes()].map(num => num.toString().padStart(2, '0')).join(':');
+                }
+                case 'currenthour':
+                case 'localhour': {
+                    return d.getHours().toString().padStart(2, '0');
+                }
+                case 'currentweek':
+                case 'localweek': {
+                    return htmlElem('UNIMPLEMENTED');
+                }
+                case 'currenttimestamp':
+                case 'localtimestamp': {
+                    return [d.getFullYear(), d.getMonth() + 1, d.getDate(), d.getHours(), d.getMinutes()]
+                        .map(num => num.toString().padStart(2, '0'))
+                        .join('');
+                }
+                // Other
+                default: {
+                    return htmlElem('UNIMPLEMENTED', `magic-word="${_name}"`, params.join('|'));
+                }
+            }
+        },
+
+        // Templates
+        'template': () => {
+            // TODO
+            const templateName = parseToken(token.firstChild);
+            return htmlElem('TRANSCLUSION', `template="${templateName}"`, '');
+        },
+
+        // Wiki-HTML
         'ext': () => {
             // Nonstandard HTML elements
             switch (token.name) {
@@ -127,12 +395,7 @@ function parseToken(token: AnyToken): string {
                 }
             }
         },
-        'double-underscore': () => {
-            // __NOTOC__, __NOINDEX__, etc
-            const { name } = token;
-            metadata[name] = true;
-            return htmlElem(name);
-        },
+        // Misc
         'quote': () => '',
 
         // Fallback //
