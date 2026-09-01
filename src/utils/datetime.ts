4dc566f0b5602e523e456bf3dae16782daabd3ec
Nixinova <nixinova@gmail.com>
2026-09-01 19:20:28 +1200

    Fix erroneous #time warnings

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -7,9 +7,9 @@
 export function convertDateTimeSyntax(pattern: string): string {
     const unsupportedChars: string[] = [];
 
-    const unsupported = (char: string, fallback: string): [string, string] => {
-        unsupportedChars.push(char);
-        return [char, fallback];
+    const unsupported = (char: string, fallback: string) => {
+        unsupportedChars.push(char)
+        return fallback;
     }
 
     const parts = pattern.split('\''); // `a'b'c` -> [a, 'b', c]
@@ -23,8 +23,8 @@ export function convertDateTimeSyntax(pattern: string): string {
         // process
         .replaceAll('y', 'yy') // year - must be before all other 'y' replacements
         .replaceAll('Y', 'yyyy') // year
-        .replaceAll(...unsupported('L', '')) // 'is leap year'
-        .replaceAll(...unsupported('o', 'yyyy')) // ISO year
+        .replaceAll('L', () => unsupported('L', '')) // 'is leap year'
+        .replaceAll('o', () => unsupported('o', 'yyyy')) // ISO year
         .replaceAll('m', 'mm') // month - must be before all other 'M' replacements
         .replaceAll('n', 'm') // month
         .replaceAll('M', 'mmm') // month
@@ -34,8 +34,8 @@ export function convertDateTimeSyntax(pattern: string): string {
         .replaceAll('D', 'ddd') // day
         .replaceAll('l', 'dddd') // day
         .replaceAll('W', 'W') // ISO week number
-        .replaceAll(...unsupported('N', '')) // ISO week day
-        .replaceAll(...unsupported('w', '')) // alternate week day
+        .replaceAll('N', () => unsupported('N', '')) // ISO week day
+        .replaceAll('w', () => unsupported('w', '')) // alternate week day
         .replaceAll('a', 'tt') // am/pm
         .replaceAll('A', 'TT') // AM/PM
         .replaceAll('h', 'hh') // hour - must be before all other 'h' replacements
@@ -44,14 +44,14 @@ export function convertDateTimeSyntax(pattern: string): string {
         .replaceAll('G', 'H') // hour
         .replaceAll('i', 'MM') // minute
         .replaceAll('s', 'ss') // second
-        .replaceAll(...unsupported('U', '')) // unix timestamp
-        .replaceAll(...unsupported('Z', '')) // timezone offset seconds - must be before all other 'Z' replacements
+        .replaceAll('U', () => unsupported('U', '')) // unix timestamp
+        .replaceAll('Z', () => unsupported('Z', '')) // timezone offset seconds - must be before all other 'Z' replacements
         .replaceAll('e', 'Z') // timezone ID
         .replaceAll('T', 'Z') // timezone abbr
-        .replaceAll(...unsupported('I', '')) // 'is DST'
+        .replaceAll('I', () => unsupported('I', '')) // 'is DST'
         .replaceAll('o', 'o') // timezone offset like +1200
         .replaceAll('P', 'p') // timezone offset like +12:00
-        .replaceAll(...unsupported('t', '')) // days in month
+        .replaceAll('t', () => unsupported('t', '')) // days in month
         .replaceAll('c', 'yyyy-mm-dd\'T\'HH:MM:ssp') // ISO 8601
         .replaceAll('r', 'ddd, dd mmm yyyy HH:MM:ss o') // RFC 5322
         // resplit
